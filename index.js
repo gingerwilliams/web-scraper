e205cb14e69343f7f813e596a94de16c9c1d8f67
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2019-07-16 12:29:16 -0400

    pull olive guarden menu html

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const req = require('request');
-const $ = require('cheerio');
+const cheerio = require('cheerio');
 
-req('http://codedemos.com/sampleblog', (error, success, html) => {
-    if (!error && success.statusCode == 200)
-        console.log('200', html);
+
+
+const options = {
+    headers: { 'user-agent': 'node.js' }
+}
+req('https://www.olivegarden.com/menu-listing/dinner', options, (error, success, html) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', success && success.statusCode); // Print the response status code if a response was received
+    console.log('body:', html); // Print the HTML for the Google homepage.
+
+    //CHEERIO
+    const $ = cheerio.load(html);
+    const content = $('.item-list-container');
+
+    //console.log(content.html());
+    // console.log(content.text());
+    // const output = content.children('div').text();
+    const output = content.html();
+
+    console.log(output);
 });
+
+
+// const olivegarden = () => {
+
+// }
+
+
--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@
 ## Dependencies
 Request - Simplified HTTP client
 
+> npm install request
+
+`const request = require('request')`
+
 ```
 var request = require('request');
 request('http://www.google.com', function (error, response, body) {
@@ -11,5 +15,10 @@ request('http://www.google.com', function (error, response, body) {
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
 ```
-
 <!-- https://www.npmjs.com/package/request -->
+
+Cheerio - Fast, flexible, and lean implementation of core jQuery designed specifically for the server
+
+## Run Application
+
+> nodemon index.js
